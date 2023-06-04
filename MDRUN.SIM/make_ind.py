cdb260ac60969659d52b88e1080277cc86d216fe
gargibiswas92 <gargibiswas92@gmail.com>
2023-06-04 14:58:18 +0300

    4th commit

---
+++ MDRUN.SIM/make_ind.py
@@ -0,0 +1,46 @@
+outfile = open("index_new.ndx", "w")
+
+last1 = input("Enter the last bead number:")
+last = int(last1)
+sp_0 = ["\n", "[ body ]","\n"]
+outfile.writelines(sp_0)
+count1 = 0 
+for j in range(1,16885):
+    count1 = count1 + 1
+    if count1 <= 15:
+        sp_1 = [str(j), " "]
+        outfile.writelines(sp_1)
+    else:
+        sp_2 = ["\n", str(j), " "]
+        outfile.writelines(sp_2)
+        count1 = 1
+        continue
+
+sp_02 = ["\n","[ tails ]","\n"]
+outfile.writelines(sp_02)
+countp = 0
+for j in range(16885,last+1):
+    countp = countp + 1
+    if countp <= 15:
+        sp_p = [str(j), " "]
+        outfile.writelines(sp_p)
+    else:
+        sp_pp = ["\n", str(j), " "]
+        outfile.writelines(sp_pp)
+        countp = 1
+        continue
+
+
+sp_01 = ["\n","[ system ]","\n"]
+outfile.writelines(sp_01)
+count11 = 0 
+for j in range(1,last+1):
+    count11 = count11 + 1
+    if count11 <= 15:
+        sp_11 = [str(j), " "]
+        outfile.writelines(sp_11)
+    else:
+        sp_21 = ["\n", str(j), " "]
+        outfile.writelines(sp_21)
+        count11 = 1
+        continue
