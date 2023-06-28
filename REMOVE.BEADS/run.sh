e15604ccbd7be52ca45301184e403eea7d5392cb
gargibiswas92 <gargibiswas92@gmail.com>
2023-06-28 17:09:59 +0300

    13th commit

--- REMOVE.BEADS/run.sh
+++ REMOVE.BEADS/run.sh
@@ -1,4 +1,4 @@
-#!/bin/csh
+#!/bin/bash
 
 echo "Enter the number of sugars you want to keep:"
 
@@ -12,8 +12,8 @@ echo "Enter the name of the .top file:"
 
 read top
 
-python mod_structure.py $gro $number
-python mod_topology.py $top $number
+python mod_structure.py "$gro" $number
+python mod_topology.py "$top" $number
 python mod_index.py $number
 python index_group_red.py
 python mod_top_sod_red.py MOD_top.top 
